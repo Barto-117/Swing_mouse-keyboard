3398d566bbec0d719f7cb72256ff512f4c0e79f8
barto-117 <bartoszkipa@gmail.com>
2021-12-02 02:28:44 +0100

    Działa, ale można to ładniej napisać (tak mi się wydaje).

--- src/MyApp.java
+++ src/MyApp.java
@@ -1,36 +1,69 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 
 public class MyApp extends JFrame {
     JButton bRun, bCancel;
     JPanel p;
-
+    static Point pos = new Point(-100,-100);
 
     public MyApp() {
 
-        setExtendedState (JFrame.MAXIMIZED_BOTH);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
         setTitle("Swing");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setLayout(null);
         setLocationRelativeTo(null);
-
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                pos.x = e.getX();
+                pos.y = e.getY();
+                System.out.println(pos.getX() + "," + pos.getY());
+            }
+        });
 
         p = new JPanel(null);
         p.setOpaque(true);
-        p.setBackground(Color.GRAY);
         add(p);
+
+        p.setFocusable(true);
+        p.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                char m = e.getKeyChar();
+                if (m == 111) {
+                    System.out.println("o");
+                    Oval o = new Oval();
+                    o.paintComponent(getGraphics());
+                }
+                if (m == 107) {
+                    System.out.println("k");
+                    Square s = new Square();
+                    s.paintComponent(getGraphics());
+                }
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
 
         bRun = new JButton("RUN");
         add(bRun);
-        bRun.setBounds(600,710,100,50);
+        bRun.setBounds(600, 710, 100, 50);
         bRun.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
                 Point x = getMousePosition();
-                if (x.getX() > bRun.getX() + 50 ){
+                if (x.getX() > bRun.getX() + 50) {
                     super.mouseEntered(e);
                     bRun.setLocation((int) (20 + Math.random() * 1400), (int) (20 + Math.random() * 710));
                 }
@@ -39,7 +72,7 @@ public class MyApp extends JFrame {
 
         bCancel = new JButton("Cancel");
         add(bCancel);
-        bCancel.setBounds(840,710,100,50);
+        bCancel.setBounds(840, 710, 100, 50);
         bCancel.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
@@ -48,10 +81,5 @@ public class MyApp extends JFrame {
             }
         });
 
-
-    }
-
-    static public void main(String[] args) {
-        EventQueue.invokeLater(MyApp::new);
     }
 }
---
+++ src/RunApp.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+public class RunApp {
+    static public void main(String[] args) {
+        EventQueue.invokeLater(MyApp::new);
+    }
+}
+
